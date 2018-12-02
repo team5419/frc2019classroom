03e693058e4322cc9a5186d9819c95dc874a6c42
Team 5499 <bhsrobot@gmail.com>
2018-12-01 16:47:32 -0800

    Added PID loop

--- src/main/kotlin/org/team5499/frc2018/classroom/Robot.kt
+++ src/main/kotlin/org/team5499/frc2018/classroom/Robot.kt
@@ -20,12 +20,58 @@
 package org.team5499.frc2018.classroom
 
 import edu.wpi.first.wpilibj.TimedRobot // this is the base class of our Robot class, and takes care of calling the periodic and init functions
-// import org.team5499.frc2018.classroom.Constants
 // import org.team5499.frc2018.classroom.utils.Utils
-// import org.team5499.frc2018.classroom.subsystems.Drivetrain
+import org.team5499.frc2018.classroom.subsystems.Drivetrain
+import edu.wpi.first.wpilibj.Timer
 
+@Suppress("MagicNumber")
 class Robot : TimedRobot() {
+    var timer = Timer()
+    var driveDistance = 96.0
+    var distToTarget = driveDistance
+    var power = distToTarget * 0.05
+    var leftPower = 0.0
+    var rightPower = 0.0
+    var eDif = 0.0
+    var firstE = 0.0
+    var lastE = 0.0
+    var timeDif = 0.0
+    var rightAngleAdd = 0.0
+    var leftAngleAdd = 0.0
     override fun robotInit() {
         super.setPeriod(Constants.UPDATE_PERIOD)
+    }
+
+    override fun disabledInit() {
+        Drivetrain.stop()
+    }
+
+    override fun autonomousInit() {
+        Drivetrain.stop()
+        Drivetrain.reset()
+    }
+
+    override fun autonomousPeriodic() {
+        distToTarget = driveDistance - ((Drivetrain.rightDistance + Drivetrain.leftDistance) / 2.0)
+        firstE = distToTarget
+        eDif = firstE - lastE
+        timeDif = timer.get()
+        var slope = eDif / timeDif
+        var angleError = 0 - gyroAngle
+        println(slope)
+        power = (distToTarget * 0.025) + (slope * 0.004)
+
+        if (power > 0.4) {
+            power = 0.4
+        }
+        if (power < -0.4) {
+            power = -0.4
+        }
+        leftPower = power
+        rightPower = power
+        Drivetrain.setPercent(leftPower * 1.0, rightPower * 1.0)
+        timer.reset()
+        timer.start()
+        lastE = distToTarget
     }
 }
