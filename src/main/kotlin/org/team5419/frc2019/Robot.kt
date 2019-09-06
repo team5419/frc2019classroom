78a4d845ee618e9e578e066fe4440a265eb5e031
Mattiwos <bmattiwos@gmail.com>
2019-09-05 18:46:24 -0700

    Subsystem + DriveTrain

--- src/main/kotlin/org/team5419/frc2019/Robot.kt
+++ src/main/kotlin/org/team5419/frc2019/Robot.kt
@@ -1,8 +1,46 @@
 package org.team5419.frc2019
 
 import edu.wpi.first.wpilibj.TimedRobot
-
+import org.team5419.fault.hardware.LazyTalonSRX
+import org.team5419.fault.hardware.LazyVictorSPX
+import org.team5419.frc2019.subsystems.DriveTrain
 class Robot : TimedRobot() {
+    //Hardware
+    private val mLeftMaster: LazyTalonSRX
+    private val mLeftSlave1: LazyVictorSPX
+    private val mLeftSlave2: LazyVictorSPX
+
+
+    private val mRightMaster: LazyTalonSRX
+    private val mRightSlave1: LazyVictorSPX
+    private val mRightSlave2: LazyVictorSPX
+    //subsystems
+    private val mDriveTrain: DriveTrain
+    init{
+        mLeftMaster = LazyTalonSRX(6)
+        mLeftSlave1 = LazyVictorSPX(7)
+        mLeftSlave2 = LazyVictorSPX(8)
+
+        mRightMaster = LazyTalonSRX(12) //8
+        mRightSlave1 = LazyVictorSPX(2) //34
+        mRightSlave2 = LazyVictorSPX(34) //34
+
+        mDriveTrain = DriveTrain(
+            mLeftMaster,
+            mLeftSlave1,
+            mLeftSlave2,
+
+            mRightMaster,
+            mRightSlave1,
+            mRightSlave2
+
+
+
+        )
+    }
+
+
+
     override fun robotInit() { //constructor
         println("Hello World from Kotlin!")
     }
---
+++ src/main/kotlin/org/team5419/frc2019/Subsystems/DriveTrain.kt
@@ -0,0 +1,33 @@
+package org.team5419.frc2019.subsystems
+
+import org.team5419.fault.Subsystem
+import org.team5419.fault.hardware.LazyTalonSRX
+import org.team5419.fault.hardware.LazyVictorSPX
+
+public class DriveTrain(
+    private val mLeftMaster: LazyTalonSRX,
+    private val mRightMaster: LazyTalonSRX,
+
+    private val mLeftSlave1: LazyVictorSPX,
+    private val mLeftSlave2: LazyVictorSPX,
+    private val mRightSlave1: LazyVictorSPX,
+    private val mRightSlave2: LazyVictorSPX
+) : Subsystem() {
+
+    companion object {
+        private const val kPositionSlot = 0
+        private const val kVelocitySlot = 1
+        private const val kTurnSlot = 2
+        private const val kVolts = 12.0
+        private const val kWeirdPigeonConversion = 3600.0 / 8192.0
+    }
+
+    public override fun update() {
+    }
+
+    public override fun reset() {
+    }
+
+    public override fun stop() {
+    }
+}
