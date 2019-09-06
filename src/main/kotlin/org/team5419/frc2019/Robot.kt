a30069531d7828e9fe5bef7baa9b5b6bbcc65177
Mattiwos <bmattiwos@gmail.com>
2019-09-05 18:02:34 -0700

    comments

--- src/main/kotlin/org/team5419/frc2019/Robot.kt
+++ src/main/kotlin/org/team5419/frc2019/Robot.kt
@@ -3,11 +3,11 @@ package org.team5419.frc2019
 import edu.wpi.first.wpilibj.TimedRobot
 
 class Robot : TimedRobot() {
-    override fun robotInit() {
+    override fun robotInit() { //constructor
         println("Hello World from Kotlin!")
     }
-    override fun robotPeriodic() {
-        
+    override fun robotPeriodic() { //loop
+
     }
 
 }
