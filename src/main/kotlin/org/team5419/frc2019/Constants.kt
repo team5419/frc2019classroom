ba275787c2d617017cf6396bda8b0bc30fdf9311
FelixMo42 <felix@tacocat.com>
2019-09-09 20:23:34 -0700

    added constants and subsystem manager

---
+++ src/main/kotlin/org/team5419/frc2019/Constants.kt
@@ -0,0 +1,14 @@
+package org.team5419.frc2019
+
+@SuppressWarnings("MagicNumber")
+public object Constants {
+    object DriveTrain {
+        public const val LEFT_MASTER_TALON_PORT = 6
+        public const val LEFT_SLAVE1_TALON_PORT = 7
+        public const val LEFT_SLAVE2_TALON_PORT = 8
+
+        public const val RIGHT_MASTER_TALON_PORT = 12
+        public const val RIGHT_SLAVE1_TALON_PORT = 2
+        public const val RIGHT_SLAVE2_TALON_PORT = 34
+    }
+}
