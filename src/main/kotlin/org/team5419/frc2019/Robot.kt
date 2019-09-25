9f25c3dbc9579444828ae906897f38744b77a10b
John McAllister <theepicpenguin121@gmail.com>
2019-09-24 17:54:35 -0800

    Testing Sensor Position + Elevator

--- src/main/kotlin/org/team5419/frc2019/Robot.kt
+++ src/main/kotlin/org/team5419/frc2019/Robot.kt
@@ -1,11 +1,12 @@
 package org.team5419.frc2019
 
-import edu.wpi.first.wpilibj.TimedRobot
-
 import org.team5419.fault.hardware.LazyTalonSRX
 import org.team5419.fault.hardware.LazyVictorSPX
-import com.ctre.phoenix.motorcontrol.ControlMode
 
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+
+import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.GenericHID.Hand
 
@@ -20,18 +21,33 @@ class Robot : TimedRobot() {
     private val mRightSlave1: LazyVictorSPX
     private val mRightSlave2: LazyVictorSPX
 
+    private val mChainLift: LazyTalonSRX
+    private val mChainBottom: LazyTalonSRX
+
     private val mXboxController: XboxController
 
+    private val mHighestPosition: Int = 100
+
     init {
+        // Left Wheel Motors
         mLeftMaster = LazyTalonSRX(12)
         mLeftSlave1 = LazyVictorSPX(2)
         mLeftSlave2 = LazyVictorSPX(3)
 
+        // Right Wheel Motors
         mRightMaster = LazyTalonSRX(6)
         mRightSlave1 = LazyVictorSPX(7)
         mRightSlave2 = LazyVictorSPX(8)
 
+        // Elevator Motors
+        mChainLift = LazyTalonSRX(5)
+        mChainBottom = LazyTalonSRX(4)
+
+        // Xbox Controller
         mXboxController = XboxController(0)
+
+        // Feedback Device Configuration
+        mChainBottom.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative)
     }
 
     override fun robotInit() {
@@ -50,22 +66,22 @@ class Robot : TimedRobot() {
     }
 
     override fun teleopPeriodic() {
+        val ePosition: Int = mChainBottom.getSelectedSensorPosition()
+        println(ePosition)
 
-        // val chainDown: Double = mXboxController.getTriggerAxis(Hand.kLeft) / 1.0
-        // val chainUp: Double = mXboxController.getTriggerAxis(Hand.kRight) / 1.0
-
-        val leftHand: Double = mXboxController.getY(Hand.kLeft) / 1
+        val leftHandY: Double = mXboxController.getY(Hand.kLeft) / 1
+        val leftHandX: Double = mXboxController.getX(Hand.kLeft) / 1
         val rightHand: Double = mXboxController.getY(Hand.kRight) / -1
 
-        mLeftMaster.set(ControlMode.PercentOutput, leftHand)
-        mLeftSlave1.set(ControlMode.PercentOutput, leftHand)
-        mLeftSlave2.set(ControlMode.PercentOutput, leftHand)
+        mLeftMaster.set(ControlMode.PercentOutput, leftHandY)
+        mLeftSlave1.set(ControlMode.PercentOutput, leftHandY)
+        mLeftSlave2.set(ControlMode.PercentOutput, leftHandY)
 
-        mRightMaster.set(ControlMode.PercentOutput, rightHand)
-        mRightSlave1.set(ControlMode.PercentOutput, rightHand)
-        mRightSlave2.set(ControlMode.PercentOutput, rightHand)
+        mRightMaster.set(ControlMode.PercentOutput, leftHandX)
+        mRightSlave1.set(ControlMode.PercentOutput, leftHandX)
+        mRightSlave2.set(ControlMode.PercentOutput, leftHandX)
 
-        // mChainLift.set(ControlMode.PercentOutput, chainUp - chainDown)
-        // mChainBottom.set(ControlMode.PercentOutput, chainUp - chainDown)
+        mChainLift.set(ControlMode.PercentOutput, rightHand)
+        mChainBottom.set(ControlMode.PercentOutput, rightHand)
     }
 }
