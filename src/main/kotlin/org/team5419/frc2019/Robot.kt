4913c7764f61608c8deec22b5cee342b49e2cebc
John McAllister <theepicpenguin121@gmail.com>
2019-11-07 18:38:15 -0800

    Moving better

--- src/main/kotlin/org/team5419/frc2019/Robot.kt
+++ src/main/kotlin/org/team5419/frc2019/Robot.kt
@@ -50,22 +50,15 @@ class Robot : TimedRobot() {
     }
 
     override fun teleopPeriodic() {
-
-        // val chainDown: Double = mXboxController.getTriggerAxis(Hand.kLeft) / 1.0
-        // val chainUp: Double = mXboxController.getTriggerAxis(Hand.kRight) / 1.0
-
         val leftHand: Double = mXboxController.getY(Hand.kLeft) / 1
-        val rightHand: Double = mXboxController.getY(Hand.kRight) / -1
+        val rightHand: Double = mXboxController.getX(Hand.kRight) / -1
 
-        mLeftMaster.set(ControlMode.PercentOutput, leftHand)
-        mLeftSlave1.set(ControlMode.PercentOutput, leftHand)
-        mLeftSlave2.set(ControlMode.PercentOutput, leftHand)
+        mLeftMaster.set(ControlMode.PercentOutput, leftHand - rightHand)
+        mLeftSlave1.set(ControlMode.PercentOutput, leftHand - rightHand)
+        mLeftSlave2.set(ControlMode.PercentOutput, leftHand - rightHand)
 
-        mRightMaster.set(ControlMode.PercentOutput, rightHand)
-        mRightSlave1.set(ControlMode.PercentOutput, rightHand)
-        mRightSlave2.set(ControlMode.PercentOutput, rightHand)
-
-        // mChainLift.set(ControlMode.PercentOutput, chainUp - chainDown)
-        // mChainBottom.set(ControlMode.PercentOutput, chainUp - chainDown)
+        mRightMaster.set(ControlMode.PercentOutput, -leftHand - rightHand)
+        mRightSlave1.set(ControlMode.PercentOutput, -leftHand - rightHand)
+        mRightSlave2.set(ControlMode.PercentOutput, -leftHand - rightHand)
     }
 }
